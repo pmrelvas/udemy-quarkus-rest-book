3ae2f8d91221e4d913905908853cb381136884a7
Pedro Relvas <pedromiguelrelvas@gmail.com>
2022-12-02 19:31:46 +0000

    Added tests

--- src/test/java/pt/pmrelvas/BookResourceTest.java
+++ src/test/java/pt/pmrelvas/BookResourceTest.java
@@ -4,18 +4,49 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static javax.ws.rs.core.HttpHeaders.ACCEPT;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+import static javax.ws.rs.core.Response.Status.OK;
+import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
 public class BookResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void shouldGetAllBooks() {
         given()
-          .when().get("/api/books")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+                .header(ACCEPT, APPLICATION_JSON)
+                .when()
+                .get("/api/books")
+                .then()
+                .statusCode(OK.getStatusCode())
+                .body("size()", is(4));
     }
 
+    @Test
+    public void shouldCountAllBooks() {
+        given()
+                .header(ACCEPT, TEXT_PLAIN)
+                .when()
+                .get("/api/books/count")
+                .then()
+                .statusCode(OK.getStatusCode())
+                .body( is("4"));
+    }
+
+    @Test
+    public void shouldGetABook() {
+        given()
+                .header(ACCEPT, APPLICATION_JSON)
+                .pathParam("id", 1)
+                .when()
+                .get("/api/books/{id}")
+                .then()
+                .statusCode(OK.getStatusCode())
+                .body("title", is("Title 01"))
+                .body("author", is("PMR 01"))
+                .body("yearOfPublication", is(2020))
+                .body("genre", is("Tech"));
+    }
 }
